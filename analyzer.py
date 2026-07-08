c51e3057b8b40e5a317a9be8f43ebe8d5e504934
anykellylourenco-cell <anykellylourenco@gmail.com>
2026-07-08 20:16:30 -0300

    Adiciona contagem de eventos INFO, WARNING e ERROR

--- analyzer.py
+++ analyzer.py
@@ -1,6 +1,18 @@
 # Projeto: Log Analyzer
 # Autor: Any Kelly
-
+info = 0
+warning = 0
+error = 0
 with open("sample.log", "r") as arquivo:
     for linha in arquivo:
-        print(linha)
+        if "INFO" in linha:
+            info += 1
+        elif "WARNING" in linha:
+            warning += 1
+        elif "ERROR" in linha:
+            error += 1
+
+print(f"Info: {info}")
+print(f"Warning: {warning}")
+print(f"Error: {error}")
+
